0d586bf8c86922d226d7223ec56cf14f745a1231
bauman-robotics <Ce1S@4D3Zdx>
2024-05-21 23:58:51 +0300

    Set time work Set Year - not work

--- Stm32411_v1/Watter_metter/Core/Src/proc.c
+++ Stm32411_v1/Watter_metter/Core/Src/proc.c
@@ -2,7 +2,6 @@
 #include "main.h"
 #include "uartProcessing.h"
 
-
 extern watter_type watter;
 extern UART_HandleTypeDef huart2;
 extern RTC_HandleTypeDef hrtc;
@@ -17,12 +16,14 @@ void Systick_Proc(void);
 void ButtonsProcessing(void);
 void SetDate(uint32_t iDate);
 uint32_t IntToHex(uint32_t iDate);
+	
+RTC_AlarmTypeDef sAlarm;  // have to be Global  !!!!!
 //====================================
 	
 void Systick_Proc(void) {	
 	static int sysCount;
 	if (sysCount %1000 == 0) {
-		//watter.flag.send_time = 1;	
+		watter.flag.send_time = 1;	
 	}
 	if ((sysCount % UART_CMD_PROCESSING_TIME_MS) == 0) {
 		watter.flag.uart_check_rx = 1;	
@@ -48,18 +49,12 @@ void Processing(void) {
 void Send_Time(void) {
 	RTC_TimeTypeDef sTime;
 	RTC_DateTypeDef sDate;
-	RTC_AlarmTypeDef sAlarm;
 	
-	HAL_RTC_GetTime(&hrtc, &sTime,              RTC_FORMAT_BIN);//RTC_FORMAT_BIN); RTC_FORMAT_BCD  //RTC_FORMAT_BCD); //
-	HAL_RTC_GetDate(&hrtc, &sDate,              RTC_FORMAT_BIN);                                   //RTC_FORMAT_BCD); //
-	HAL_RTC_GetAlarm(&hrtc,&sAlarm,RTC_ALARM_A, RTC_FORMAT_BIN);                                   //RTC_FORMAT_BCD); //
+	HAL_RTC_GetTime(&hrtc, &sTime,              RTC_FORMAT_BIN);// RTC_FORMAT_BCD
+	HAL_RTC_GetDate(&hrtc, &sDate,              RTC_FORMAT_BIN);                  
+	HAL_RTC_GetAlarm(&hrtc,&sAlarm,RTC_ALARM_A, RTC_FORMAT_BIN);                  
 	
-//	sprintf(buf,"Date %d-%d-20%d Time %d:%d:%d  Alarm Time %d:%d:%d%c", 
-//						sDate.Date, sDate.Month, sDate.Year,
-//						sTime.Hours, sTime.Minutes, sTime.Seconds, 
-//						sAlarm.AlarmTime.Hours, sAlarm.AlarmTime.Minutes, sAlarm.AlarmTime.Seconds, 13);
-
-	sprintf(buf,"Date %d-%d-20%d Time %d:%d:%d  Alarm Time %d:%d:%d    Hot:%d    Cold:%d%c", 
+	sprintf(buf,"Date %02d-%02d-20%d Time %02d:%02d:%02d  Alarm Time %02d:%02d:%02d    Hot:%d    Cold:%d%c", 
 						sDate.Date, sDate.Month, sDate.Year,
 						sTime.Hours, sTime.Minutes, sTime.Seconds, 
 						sAlarm.AlarmTime.Hours, sAlarm.AlarmTime.Minutes, sAlarm.AlarmTime.Seconds,
@@ -70,41 +65,44 @@ void Send_Time(void) {
 }
 //====================================
 
+
 void HAL_RTC_AlarmAEventCallback(RTC_HandleTypeDef *hrtc) { 
-  RTC_AlarmTypeDef sAlarm;
-  HAL_RTC_GetAlarm(hrtc,&sAlarm,RTC_ALARM_A,FORMAT_BIN);	
-  if(sAlarm.AlarmTime.Seconds>58) {
+  HAL_RTC_GetAlarm(hrtc, &sAlarm, RTC_ALARM_A,FORMAT_BIN);	
+
+  if(sAlarm.AlarmTime.Seconds > 58) {
     sAlarm.AlarmTime.Seconds=0;
-  }else{
+  } 
+	else {
     sAlarm.AlarmTime.Seconds=sAlarm.AlarmTime.Seconds + 1;
   }
+
   while(HAL_RTC_SetAlarm_IT(hrtc, &sAlarm, FORMAT_BIN)!=HAL_OK){}
-	//while(HAL_RTC_SetAlarm_IT(hrtc, &sAlarm, RTC_FORMAT_BCD)!=HAL_OK){}	
 }
 //====================================
 
 void SetDate(uint32_t iDate) {
 	
 	RTC_DateTypeDef sDate;
+	RTC_TimeTypeDef sTime;
 	
 	uint32_t strHex = IntToHex(iDate);
-	
+	//uint32_t strHex = iDate; 
 
 	uint8_t year = 0;
 	uint8_t mounth = 0;
 	uint8_t date = 0;	
 	//						sDate.Date, sDate.Month, sDate.Year,
-//	year = (uint8_t)(iDate / 10000); 
-//	mounth = (uint8_t)((iDate - year * 10000) / 100);
-//	date = (uint8_t)((iDate - year * 10000) - mounth * 100);
+	year = (uint8_t)(iDate / 10000); 
+	mounth = (uint8_t)((iDate - year * 10000) / 100);
+	date = (uint8_t)((iDate - year * 10000) - mounth * 100);
 	
 //	uint32_t Year   = (strHex & 0xff0000) >> (8*2); 
 //	uint32_t Mounth = (strHex & 0xff00)   >> (8*1); 
 //	uint32_t Date   = (strHex & 0x00ff)   >> (8*0); 
 	
-	year =   (uint8_t)((strHex & 0xff0000) >> (8*2)); 
-	mounth = (uint8_t)((strHex & 0xff00)   >> (8*1)); 
-	date =   (uint8_t)((strHex & 0x00ff)   >> (8*0)); 
+//	year =   (uint8_t)((strHex & 0xff0000) >> (8*2)); 
+//	mounth = (uint8_t)((strHex & 0xff00)   >> (8*1)); 
+//	date =   (uint8_t)((strHex & 0x00ff)   >> (8*0)); 
 
 	
 	sDate.Year = year; 
@@ -113,7 +111,15 @@ void SetDate(uint32_t iDate) {
 
 //AL_StatusTypeDef HAL_RTC_SetDate(RTC_HandleTypeDef *hrtc, RTC_DateTypeDef *sDate, uint32_t Format);
 	
-	HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BCD);//RTC_FORMAT_BIN);
+	if (HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BIN)) //RTC_FORMAT_BCD);//RTC_FORMAT_BIN);
+	{
+	  Error_Handler();
+	}	
+	
+	if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BIN) != HAL_OK)
+	{
+	  Error_Handler();
+	}
 }
 //====================================
 uint32_t IntToHex(uint32_t iDate) {
